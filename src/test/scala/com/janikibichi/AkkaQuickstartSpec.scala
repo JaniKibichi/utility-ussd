9f9ba4da6ffbe1e4ef29b0d0285d62f8f39e29e9
Graham Ingokho <janikibichi@gmail.com>
2021-06-17 17:32:08 +0300

    set up of akka api

---
+++ src/test/scala/com/janikibichi/AkkaQuickstartSpec.scala
@@ -0,0 +1,25 @@
+//#full-example
+package com.janikibichi
+
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import com.janikibichi.Greeter.Greet
+import com.janikibichi.Greeter.Greeted
+import org.scalatest.wordspec.AnyWordSpecLike
+
+//#definition
+class AkkaQuickstartSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
+//#definition
+
+  "A Greeter" must {
+    //#test
+    "reply to greeted" in {
+      val replyProbe = createTestProbe[Greeted]()
+      val underTest = spawn(Greeter())
+      underTest ! Greet("Santa", replyProbe.ref)
+      replyProbe.expectMessage(Greeted("Santa", underTest.ref))
+    }
+    //#test
+  }
+
+}
+//#full-example
